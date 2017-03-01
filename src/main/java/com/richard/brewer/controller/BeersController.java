21e95bea1220c5e4ed9058443f1b2caf3c74ce36
Ricardolv <richardluizv@gmail.com>
2017-03-01 05:50:08 -0300

    Começando o cadastro de cerveja

--- src/main/java/com/richard/brewer/controller/BeersController.java
+++ src/main/java/com/richard/brewer/controller/BeersController.java
@@ -1,13 +1,27 @@
 package com.richard.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.richard.brewer.model.Beer;
+
 @Controller
+@RequestMapping("/beers")
 public class BeersController {
 	
-	@RequestMapping("/beers/new")
-	private String newBeer() {
+	@GetMapping("/new")
+	public String newBeer() {
+		return "beer/RegisterBeers";
+	}
+	
+	@PostMapping("/new")
+	public String register(Beer beer) {
+		
+		System.out.println("-> sku " + beer.getSku());
+		System.out.println("-> cadastrar");
+		
 		return "beer/RegisterBeers";
 	}
 
