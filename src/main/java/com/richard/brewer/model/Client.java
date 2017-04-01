c4e687382a5842a9b993b755545b77edaa8e68f2
Ricardolv <richardluizv@gmail.com>
2017-04-01 20:31:50 -0300

    prototipando tela cadastro cliente

---
+++ src/main/java/com/richard/brewer/model/Client.java
@@ -0,0 +1,20 @@
+package com.richard.brewer.model;
+
+public class Client {
+	
+	private String name;
+	private TypePerson typePerson;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public TypePerson getTypePerson() {
+		return typePerson;
+	}
+	public void setTypePerson(TypePerson typePerson) {
+		this.typePerson = typePerson;
+	}
+}
---
+++ src/main/java/com/richard/brewer/model/TypePerson.java
@@ -0,0 +1,5 @@
+package com.richard.brewer.model;
+
+public enum TypePerson {
+
+}
