aae698683e7dc6f0be0063d05abf4a483d35a8e6
Ricardolv <richardluizv@gmail.com>
2017-04-08 14:52:14 -0300

    mensagem cadastro cerveja

--- src/main/java/com/richard/brewer/controller/converter/StyleConverter.java
+++ src/main/java/com/richard/brewer/controller/converter/StyleConverter.java
@@ -1,15 +1,21 @@
 package com.richard.brewer.controller.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
+
 import com.richard.brewer.model.Style;
 
 public class StyleConverter implements Converter<String, Style> {
 
 	@Override
 	public Style convert(String code) {
-		Style style = new Style();
-		style.setCode(Long.valueOf(code));
-		return style;
+		
+		if (!StringUtils.isEmpty(code)) {
+			Style style = new Style();
+			style.setCode(Long.valueOf(code));
+			return style;
+		}
+		return null;
 	}
 
 }
