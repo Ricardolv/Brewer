6869870c85d8b3f79c3d934dbcd9b5f7679bcf0e
Ricardolv <richardluizv@gmail.com>
2017-04-28 00:36:34 -0300

    Melhorando a disponibilidade da aplicação - retorno assíncrono

--- src/main/java/com/richard/brewer/controller/PhotosControler.java
+++ src/main/java/com/richard/brewer/controller/PhotosControler.java
@@ -4,18 +4,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.richard.brewer.storage.PhotoStorageRunnable;
 
 @RestController
 @RequestMapping("/photos")
 public class PhotosControler {
 	
 	@PostMapping
-	public String upload(@RequestParam("files[]") MultipartFile[] files) {
+	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files) {
+		
+		DeferredResult<String> result = new DeferredResult<>();
+		
+		Thread thread = new Thread(new PhotoStorageRunnable(files, result));
+		thread.start();
 		
 		
-		
-		return "ok";
+		return result;
 	}
 
 }
