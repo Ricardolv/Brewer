54612b206fe574e18688391e7dbe954a52c1b486
Ricardolv <richardluizv@gmail.com>
2017-04-30 14:54:40 -0300

    Desafio: Configurando máscara do CEP com jQuery

--- src/main/resources/static/js/brewer.js
+++ src/main/resources/static/js/brewer.js
@@ -42,6 +42,20 @@ Brewer.MaskPhoneNumber = (function() {
 	
 }());
 
+Brewer.MaskCep = (function() {
+	
+	function MaskCep() {
+		this.inputCep = $('.js-cep');
+	}
+	
+	MaskCep.prototype.enable = function() {
+		this.inputCep.mask('00.000-000');
+	}
+	
+	return MaskCep;
+	
+}());
+
 $(function() {
 	
 	var maskMoney = new Brewer.MaskMoney();
@@ -49,4 +63,7 @@ $(function() {
 	
 	var maskPhoneNumber = new Brewer.MaskPhoneNumber();
 	maskPhoneNumber.enable();
+	
+	var maskCep = new Brewer.MaskCep();
+	maskCep.enable();
 });
