95fcc8b04784080194d46653f0cac8c57fc6400f
Ricardolv <richardluizv@gmail.com>
2017-05-01 13:54:30 -0300

    Selecionado cidades pelo estado com Ajax

--- src/main/java/com/richard/brewer/model/City.java
+++ src/main/java/com/richard/brewer/model/City.java
@@ -3,12 +3,15 @@ package com.richard.brewer.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "city")
@@ -21,8 +24,9 @@ public class City implements Serializable {
 	private Long code;
 	private String name;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "code_state")
+	@JsonIgnore
 	private State state;
 	
 	public Long getCode() {
