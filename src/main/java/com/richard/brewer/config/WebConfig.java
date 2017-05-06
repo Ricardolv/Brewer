f9c12e080e47e73e1dc017b5d0ce15d01eeea38f
Ricardolv <richardluizv@gmail.com>
2017-05-06 10:16:26 -0300

    Input e componente para Data

--- src/main/java/com/richard/brewer/config/WebConfig.java
+++ src/main/java/com/richard/brewer/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.richard.brewer.config;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -9,10 +10,13 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
@@ -96,7 +100,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		
 		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
 		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
-
+		
+		DateTimeFormatterRegistrar dateTimeFormatter = new DateTimeFormatterRegistrar();
+		dateTimeFormatter.setDateFormatter(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		dateTimeFormatter.registerFormatters(conversionService);
 		
 		return conversionService;
 	}
@@ -116,6 +123,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		cacheManager.setCacheBuilder(cacheBuilder);
 		
 		return cacheManager;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+		bundle.setBasename("classpath:/messages");
+		bundle.setDefaultEncoding("UTF-8"); // http://www.utf8-chartable.de/
+		return bundle;
 	}
 
 }
--- src/main/resources/static/js/brewer.js
+++ src/main/resources/static/js/brewer.js
@@ -56,6 +56,26 @@ Brewer.MaskZipCode = (function() {
 	
 }());
 
+Brewer.MaskDate = (function() {
+	
+	function MaskDate() {
+		this.inputDate = $('.js-date');
+		this.inputDate.date
+	}
+	
+	MaskDate.prototype.enable = function() {
+		this.inputDate.mask('00/00/0000');
+		this.inputDate.datepicker({
+			 orientation: 'bottom',
+			 language: 'pt-BR',
+			 autoclose: true
+		});
+	}
+	
+	return MaskDate;
+	
+}());
+
 $(function() {
 	
 	var maskMoney = new Brewer.MaskMoney();
@@ -66,4 +86,7 @@ $(function() {
 	
 	var maskZipCode = new Brewer.MaskZipCode();
 	maskZipCode.enable();
+	
+	var maskDate = new Brewer.MaskDate();
+	maskDate.enable();
 });
