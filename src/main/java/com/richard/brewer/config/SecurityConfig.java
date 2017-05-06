951207f2b20d1dc41fc982ef2c2d654160abf460
Ricardolv <richardluizv@gmail.com>
2017-05-06 14:20:03 -0300

    Tela de login customizada

--- src/main/java/com/richard/brewer/config/SecurityConfig.java
+++ src/main/java/com/richard/brewer/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.richard.brewer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,16 +19,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("admin").password("admin").roles("REGISTER_CLIENT");
 	}
 	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//		.authorizeRequests()
-//			.anyRequest().authenticated()
-//		.and()
-//			.formLogin()
-//		.and()
-//			.csrf().disable();
-//	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		.antMatchers("/layout/**")
+		.antMatchers("/images/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			
+			.anyRequest().authenticated()
+		.and()
+			.formLogin()
+			.loginPage("/login").permitAll()
+		.and()
+			.csrf().disable();
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
