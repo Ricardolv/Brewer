3b513270eb8e6b8551a39e66f9ef61c42fc3bece
Ricardolv <richardluizv@gmail.com>
2017-05-12 23:50:25 -0300

    Melhorando usabilidade da página de pesquisa de usuários

--- src/main/resources/static/javascripts/multi-selection.js
+++ src/main/resources/static/javascripts/multi-selection.js
@@ -5,10 +5,13 @@ Brewer.SelectionMulti = (function() {
 	function SelectionMulti() {
 		this.statusBtn = $('.js-status-btn');
 		this.selectionCheckbox = $('.js-selection');
+		this.selectionCheckboxAll= $('.js-selection-all');
 	}
 	
 	SelectionMulti.prototype.init = function() {
 		this.statusBtn.on('click', onStatusBtnClicado.bind(this));
+		this.selectionCheckboxAll.on('click', onSlectionAllClicked.bind(this));
+		this.selectionCheckbox.on('click', onSelectionClicked.bind(this));
 	}
 	
 	function onStatusBtnClicado(event) {
@@ -37,11 +40,29 @@ Brewer.SelectionMulti = (function() {
 		
 	}
 	
+	function onSlectionAllClicked() {
+		var status = this.selectionCheckboxAll.prop('checked');
+		this.selectionCheckbox.prop('checked', status);
+		statusButtonAction.call(this, status);
+	}
+
+	function onSelectionClicked() {
+		var selectionCheckboxCheckeds = this.selectionCheckbox.filter(':checked');
+		this.selectionCheckboxAll.prop('checked', selectionCheckboxCheckeds.length >= this.selectionCheckbox.length);
+		statusButtonAction.call(this, selectionCheckboxCheckeds.length);
+	}
+	
+	function statusButtonAction(active) {
+		active ? this.statusBtn.removeClass('disabled') : this.statusBtn.addClass('disabled');
+	}
+	
 	return SelectionMulti;
 	
 }());
 
 $(function() {
+	
 	var selectionMulti = new Brewer.SelectionMulti();
 	selectionMulti.init();
+	
 });
